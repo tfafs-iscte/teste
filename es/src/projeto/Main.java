54665d51248c26a27aa3713d708556951e60e935
tfafs-iscte <tfafs@iscte-iul.pt>
2021-02-26 20:51:02 +0000

    classe main

---
+++ es/src/projeto/Main.java
@@ -0,0 +1,11 @@
+package projeto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("ola");
+	}
+
+}
--- es/src/projeto/teste.java
+++
@@ -1,9 +0,0 @@
-package projeto;
-
-public class teste {
-
-	
-	teste3
-	
-	
-}
